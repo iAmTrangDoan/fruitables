2aec314faadfc811992ef7fd7625d5699f1133e4
Trang Doan <doantrangltt@gmail.com>
2025-12-17 23:15:23 +0700

    update connection host

--- config/config.php
+++ config/config.php
@@ -1,19 +1,24 @@
 <?php
-    // // Ngăn truy cập trực tiếp vào file
-    // if (!defined('ACCESS_ALLOWED')) {
-    //     die('Direct access not allowed');
-    // } 
+    // Ngăn truy cập trực tiếp vào file
+    if (!defined('ACCESS_ALLOWED')) {
+        die('Direct access not allowed');
+    } 
 
-    $host='localhost';
-    $dbname='organic_store';
-    $username='root';
-    $password='';
+    // $host='localhost';
+    // $dbname='organic_store';
+    // $username='root';
+    // $password='';
+
+    $host='sql305.infinityfree.com';
+    $dbname='if0_40280315_organic_store';
+    $username='if0_40280315';
+    $password='Doantrang130613';
     try {
         // Tạo kết nối PDO
         $pdo=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4",$username,$password);
         // Thiết lập chế độ lỗi
         $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
+        $pdh->query("set names 'utf8'");
     }catch(PDOException $e){
         // echo"Error Connection".$e->getMessage();
         die("Lỗi kết nối DB: " . $e->getMessage());
